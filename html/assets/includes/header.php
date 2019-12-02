13a535163f99d3d1fd1dc7fedd640dee9c1649f7
Jerad Bowles <jeradbowles@gmail.com>
2019-12-02 10:21:10 -0600

    Added Folders & updated header.php

--- html/assets/includes/header.php
+++ html/assets/includes/header.php
@@ -4,7 +4,19 @@
         <ul class="main-nav">
             <li><a href="index.html">Home</a></li>
             <li><a href="sustainability.html">Sustainability</a></li>
+                <ul class="sustain">
+                    <li><a href="#renew-energy">Renewable Energy</a></li>
+                    <li><a href="#food">Food</a></li>
+                    <li><a href="#water">Water Collection</a></li>
+                    <li><a href="#recycle">Recycling</a></li>
+                </ul>
             <li><a href="construction.html">Construction</a></li>
+                <ul class="construction">
+                    <li><a href="#design">Design</a></li>
+                    <li><a href="#materials">Materials</a></li>
+                    <li><a href="#building">Building</a></li>
+                    <li><a href="#building-code">Building Codes</a></li>
+                </ul>
             <li><a href="econews.html">Eco News</a></li>
             <li><a href="resources.html">Resources</a></li>
             <li><a href="faq.html">FAQ</a></li>
