622211f868e52b8dc9c9d53e00cc50cae7f9456f
Savannah S <analeeskinner@gmail.com>
2019-12-02 10:25:03 -0600

    fixed index.php

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
 <?php 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-// mistakes were made
->>>>>>> Stashed changes
 header('Location: html/index.php');
-=======
-header('Locaion: /html/index.php');
->>>>>>> Stashed changes
 ?>
