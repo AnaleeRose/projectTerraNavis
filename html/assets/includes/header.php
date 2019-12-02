39488e79c34b11ed3f9389b2410d0a1ee9b81372
Savannah S <analeeskinner@gmail.com>
2019-12-02 10:31:18 -0600

    header updated to only show sublinks on that page

--- html/assets/includes/header.php
+++ html/assets/includes/header.php
@@ -1,26 +1,39 @@
 <header id="header">
-    <a href="index.html"><img id="headerlogo" src="images/" alt="Terra Navis Living Logo"></a>
+    <a href="index.php"><img id="headerlogo" src="images/" alt="Terra Navis Living Logo"></a>
     <nav id="main-nav">
         <ul class="main-nav">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="sustainability.html">Sustainability</a></li>
-                <ul class="sustain">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="sustainability.php">Sustainability</a></li>
+            <?php 
+            // change the title in quotes below to match whatever the page title is on that page, all lower case in the quotes pls
+            // also added the dropdownUl class for the styles that match both (like fo js to target it for special effects!)
+            if (strtolower($page_title) == 'sustainability') { 
+            ?>
+                <ul class="sustain dropdownUl">
                     <li><a href="#renew-energy">Renewable Energy</a></li>
                     <li><a href="#food">Food</a></li>
                     <li><a href="#water">Water Collection</a></li>
                     <li><a href="#recycle">Recycling</a></li>
                 </ul>
-            <li><a href="construction.html">Construction</a></li>
-                <ul class="construction">
+            <?php } ?>
+
+            <li><a href="construction.php">Construction</a></li>
+
+            <?php 
+            // change the title in quotes below to match whatever the page title is on that page, all lower case in the quotes pls
+            if (strtolower($page_title) == 'construction') { 
+            ?>
+                <ul class="construction dropdownUl">
                     <li><a href="#design">Design</a></li>
                     <li><a href="#materials">Materials</a></li>
                     <li><a href="#building">Building</a></li>
                     <li><a href="#building-code">Building Codes</a></li>
                 </ul>
-            <li><a href="econews.html">Eco News</a></li>
-            <li><a href="resources.html">Resources</a></li>
-            <li><a href="faq.html">FAQ</a></li>
-            <li><a href="contact.html">Contact</a></li>
+            <?php } ?>
+            <li><a href="econews.php">Eco News</a></li>
+            <li><a href="resources.php">Resources</a></li>
+            <li><a href="faq.php">FAQ</a></li>
+            <li><a href="contact.php">Contact</a></li>
         </ul>
     </nav>
 </header>
