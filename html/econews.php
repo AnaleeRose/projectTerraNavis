18d0cded7df6e83b6553e26056fcd46a5fc45cd9
Jerad Bowles <jeradbowles@gmail.com>
2019-12-03 10:40:24 -0600

    Reset CSS styles & econews.php completed

--- html/econews.php
+++ html/econews.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Terra Navis | Construction</title>
+    <title>Terra Navis | Eco News</title>
     <meta name="description" content="Earthship bio-friendly homes">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/styles.css">
@@ -17,25 +17,48 @@
 </div>
 
 <!-- Main body content -->
-<div id="sec1">
-    <h1 class="sec-title">Designing Your Earthship</h1>
+<div class="article1">
+    <img class="article-img" src="" alt="Article Image">
+    <p>November 22nd, 2019</p>
+    <h1 class="sec-title">Title of Article</h1>
     <p>An Earthship is a brand of passive solar earth shelter that is made of both natural and upcycled materials such as earth-packed tires, recycled bottles and reclaimed materials. Pioneered by architect Michael Reynolds, with the goal of creating a home that would do three things. First, it would utilize sustainable architecture, and material indigenous to the local area or recycled materials wherever possible. Second, the homes would rely on natural energy sources and be independent from the “grid”. Third, it would be feasible for a person with no specialized construction skills to build.</p>
+    <p class="readmore">Read More >></p>
 </div>
-
-<div id="sec2">
-    <h1 class="sec-title">Building Materials</h1>
-    <p>There’s green, and then there’s green…. Michael Reynolds dreamed of creating an ultra eco-friendly way of living with his Earthship designs. These principles led to an extreme minimum in waste by using such methods as building with mostly up cycled materials, collecting rain water, using solar and wind energy, implementation of black and grey water systems to feed into outdoor gardens to aid in the growing of ones own food. When all of these concepts are brought together in a single design, you get one of the most eco-friendly home options available to us today.</p>
+<div class="article2">
+    <img class="article-img" src="" alt="Article Image">
+    <p>November 22nd, 2019</p>
+    <h1 class="sec-title">Title of Article</h1>
+    <p>An Earthship is a brand of passive solar earth shelter that is made of both natural and upcycled materials such as earth-packed tires, recycled bottles and reclaimed materials. Pioneered by architect Michael Reynolds, with the goal of creating a home that would do three things. First, it would utilize sustainable architecture, and material indigenous to the local area or recycled materials wherever possible. Second, the homes would rely on natural energy sources and be independent from the “grid”. Third, it would be feasible for a person with no specialized construction skills to build.</p>
+    <p class="readmore">Read More >></p>
 </div>
-
-<div id="sec3">
-    <h1 class="sec-title">Building Your Earthship</h1>
-        <p>Earthships contain both recycled and natural materials, earning them the label of “carbon-zero” homes. Walls are constructed using the “rammed-earth” method with old tires as the building blocks and glass bottles surrounded by concrete to create bottle-walls that bring natural light into the structure. The front of the earthship contains large glass panels to bring more light in, while also creating a section to act as a greenhouse for the growing of vegetation year round lending to a sustainable source of food. With traditional plumbing and electrical running throughout the earthship, albeit powered by solar and wind energy, this allows for modern living in ultra green home! </p>
+<div class="article3">
+    <img class="article-img" src="" alt="Article Image">
+    <p>November 22nd, 2019</p>
+    <h1 class="sec-title">Title of Article</h1>
+    <p>An Earthship is a brand of passive solar earth shelter that is made of both natural and upcycled materials such as earth-packed tires, recycled bottles and reclaimed materials. Pioneered by architect Michael Reynolds, with the goal of creating a home that would do three things. First, it would utilize sustainable architecture, and material indigenous to the local area or recycled materials wherever possible. Second, the homes would rely on natural energy sources and be independent from the “grid”. Third, it would be feasible for a person with no specialized construction skills to build.</p>
+    <p class="readmore">Read More >></p>
 </div>
-
-<div id="sec4">
-        <h1 class="sec-title">Building It To Code</h1>
-            <p>Earthships contain both recycled and natural materials, earning them the label of “carbon-zero” homes. Walls are constructed using the “rammed-earth” method with old tires as the building blocks and glass bottles surrounded by concrete to create bottle-walls that bring natural light into the structure. The front of the earthship contains large glass panels to bring more light in, while also creating a section to act as a greenhouse for the growing of vegetation year round lending to a sustainable source of food. With traditional plumbing and electrical running throughout the earthship, albeit powered by solar and wind energy, this allows for modern living in ultra green home! </p>
-    </div>
+<div class="article4">
+    <img class="article-img" src="" alt="Article Image">
+    <p>November 22nd, 2019</p>
+    <h1 class="sec-title">Title of Article</h1>
+    <p>An Earthship is a brand of passive solar earth shelter that is made of both natural and upcycled materials such as earth-packed tires, recycled bottles and reclaimed materials. Pioneered by architect Michael Reynolds, with the goal of creating a home that would do three things. First, it would utilize sustainable architecture, and material indigenous to the local area or recycled materials wherever possible. Second, the homes would rely on natural energy sources and be independent from the “grid”. Third, it would be feasible for a person with no specialized construction skills to build.</p>
+    <p class="readmore">Read More >></p>
+</div>
+<div class="article5">
+    <img class="article-img" src="" alt="Article Image">
+    <p>November 22nd, 2019</p>
+    <h1 class="sec-title">Title of Article</h1>
+    <p>An Earthship is a brand of passive solar earth shelter that is made of both natural and upcycled materials such as earth-packed tires, recycled bottles and reclaimed materials. Pioneered by architect Michael Reynolds, with the goal of creating a home that would do three things. First, it would utilize sustainable architecture, and material indigenous to the local area or recycled materials wherever possible. Second, the homes would rely on natural energy sources and be independent from the “grid”. Third, it would be feasible for a person with no specialized construction skills to build.</p>
+    <p class="readmore">Read More >></p>
+</div>
+<div class="article6">
+    <img class="article-img" src="" alt="Article Image">
+    <p>November 22nd, 2019</p>
+    <h1 class="sec-title">Title of Article</h1>
+    <p>An Earthship is a brand of passive solar earth shelter that is made of both natural and upcycled materials such as earth-packed tires, recycled bottles and reclaimed materials. Pioneered by architect Michael Reynolds, with the goal of creating a home that would do three things. First, it would utilize sustainable architecture, and material indigenous to the local area or recycled materials wherever possible. Second, the homes would rely on natural energy sources and be independent from the “grid”. Third, it would be feasible for a person with no specialized construction skills to build.</p>
+    <p class="readmore">Read More >></p>
+</div>
 
 <!------ Footer ------------>
 ​<?php require './assets/includes/footer.php'; ?>
