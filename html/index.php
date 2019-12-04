52dcfb9ff4340744b21dc84c7508c1f5e099df1b
Nick Ferrari <njferrari14@gmail.com>
2019-12-04 08:40:52 -0600

    changed the first sentence of the third paragraph because it was copied from another site. Hyperlinked the source. Fixed some errors

--- html/index.php
+++ html/index.php
@@ -19,23 +19,23 @@
 <!-- Main body content -->
 <div id="mainpg-sec1">
     <h1 class="sec-title">What is an Earthship?</h1>
-    <p>Earthships are a type of passive solar earth shelter first conceptualized by architect Michael Reynolds. They are built with a combination of upcycled and natural resources, like recycled glass bottles, tires packed with earth, and other, reclaimed materials. Reynolds developed the concept to realize three goals:
-        <ul>
-            <li><p>Utilize sustainable architecture and materials which can be recycled or naturally sourced from local environments</p></li>
-            <li><p>Build an "off-the-grid" home that would rely entirely on natural energy sources.</p></li>
-            <li><p>Make the building process feasible for a person without any specialized construction skills.</p></li>
-        </ul>
+    <p>Earthships are a type of passive solar earth shelter first conceptualized by architect Michael Reynolds. They are built with a combination of upcycled and natural resources, like recycled glass bottles, tires packed with earth, and other reclaimed materials. Reynolds developed the concept to realize three goals:
     </p>
+    <ul>
+        <li><p>Utilize sustainable architecture and materials which can be recycled or naturally sourced from local environments</p></li>
+        <li><p>Build an "off-the-grid" home that would rely entirely on natural energy sources.</p></li>
+        <li><p>Make the building process feasible for a person without any specialized construction skills.</p></li>
+    </ul>
 </div>
 ​
 <div id="mainpg-sec2">
     <h1 class="sec-title">Sustainable Living</h1>
-    <p>There’s green, and then there’s <strong>green</strong>. Michael Reynolds dreamed of creating an ultra eco-friendly way of living with his Earthship designs. These principles led to an extreme minimum in waste by using such methods as building with mostly upcycled materials, collecting rain water, using solar and wind energy, implementation of black and grey water systems to feed into outdoor gardens to aid in the growing of ones own food. When all of these concepts are brought together in a single design, you get one of the most eco-friendly home options available to us today.</p>
+    <p>There’s green, and then there’s <strong>green</strong>. Michael Reynolds dreamed of creating an ultra eco-friendly way of living with his Earthship designs. These principles led to an extreme minimum in waste by using methods such as building with mostly upcycled materials, collecting rain water, using solar and wind energy, and implementation of black and grey water systems to feed into outdoor gardens to aid in the growing of ones own food. When all of these concepts are brought together in a single design, you get one of the most eco-friendly home options available today.</p>
 </div>
 ​
 <div id="mainpg-sec3">
     <h1 class="sec-title">Building Your Earthship</h1>
-        <p>Earthships contain both recycled and natural materials, earning them the label of “carbon-zero” homes. Walls are constructed using the “rammed-earth” method with old tires as the building blocks and glass bottles surrounded by concrete to create bottle-walls that bring natural light into the structure. The front of the earthship contains large glass panels to bring more light in, while also creating a section to act as a greenhouse for the growing of vegetation year round lending to a sustainable source of food. With traditional plumbing and electrical running throughout the earthship, albeit powered by solar and wind energy, this allows for modern living in ultra green home! </p>
+        <p>Because earthships are built using a mix of natural and upcycled materials, they can be considered "<a href="https://www.smartcitiesdive.com/ex/sustainablecitiescollective/sustainable-homes-earthship/1156549/">carbon-zero</a>" homes. Walls are constructed using the “rammed-earth” method with old tires as the building blocks and glass bottles surrounded by concrete to create bottle-walls that bring natural light into the structure. The front of the earthship contains large glass panels to bring more light in, while also creating a section to act as a greenhouse for the growing of vegetation year round lending to a sustainable source of food. With traditional plumbing and electrical systems running throughout the earthship, albeit powered by solar and wind energy, this allows for modern living in an ultra green home! </p>
 </div>
 
 <!------ Footer ------------>
