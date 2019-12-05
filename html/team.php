974b764ca5c3c230ac1846d4c86bd51e0585b718
Jerad Bowles <jeradbowles@gmail.com>
2019-12-05 09:01:51 -0600

    Added team.php page

---
+++ html/team.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Terra Navis | Our Team</title>
+    <meta name="description" content="Earthship bio-friendly homes">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+<!------ Header ------------>
+<?php require './assets/includes/header.php'; ?>
+
+<!-- Main body content -->
+<h1 class="team-title">Meet Our Team</h1>
+
+<div class="card1">
+    <img class="portrait" src="" alt="Portrait">
+    <h2 class="question">Nicholas Ferrari</h2>
+    <h3 class="question">Project Manager & SEO</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus cras adipiscing enim eu turpis egestas pretium. Ac felis donec et odio pellentesque diam. Aliquam faucibus purus in massa tempor nec feugiat nisl. Dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu. Semper feugiat nibh sed pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliq.</p>
+</div>
+
+<div class="card2">
+    <img class="portrait" src="" alt="Portrait">
+    <h2 class="question">Jerad Bowles</h2>
+    <h3 class="question">Front-end Developer & Content Specialist</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus cras adipiscing enim eu turpis egestas pretium. Ac felis donec et odio pellentesque diam. Aliquam faucibus purus in massa tempor nec feugiat nisl. Dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu. Semper feugiat nibh sed pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliq.</p>
+</div>
+
+<div class="card3">
+    <img class="portrait" src="" alt="Portrait">
+    <h2 class="question">Savannah Skinner</h2>
+    <h3 class="question">Back-end Developer & UX</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus cras adipiscing enim eu turpis egestas pretium. Ac felis donec et odio pellentesque diam. Aliquam faucibus purus in massa tempor nec feugiat nisl. Dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu. Semper feugiat nibh sed pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliq.</p>
+</div>
+
+<div class="card4">
+    <img class="portrait" src="" alt="Portrait">
+    <h2 class="question">Noshin Rahman</h2>
+    <h3 class="question">Design</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus cras adipiscing enim eu turpis egestas pretium. Ac felis donec et odio pellentesque diam. Aliquam faucibus purus in massa tempor nec feugiat nisl. Dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu. Semper feugiat nibh sed pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliq.</p>
+</div>
+
+<div class="card5">
+    <p>Chapter: 03-0042</p>
+    <p>Theme: BPA Bio Friendly Home</p>
+    <p>School: Francis Tuttle Technology Center</p>
+    <p>Location: Oklahoma City, OK</p>
+</div>
+
+<!------ Footer ------------>
+​<?php require './assets/includes/footer.php'; ?>
+
+</body>
+</html>
