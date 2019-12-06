9e157cc887cc2cdab8918219c7ab0813a67fc01e
Jerad Bowles <jeradbowles@gmail.com>
2019-12-06 14:03:10 -0600

    updated team.php bio

--- html/team.php
+++ html/team.php
@@ -18,7 +18,7 @@
     <img class="portrait" src="" alt="Portrait">
     <h2 class="name">Nicholas Ferrari</h2>
     <h3 class="role">Project Manager & SEO</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus cras adipiscing enim eu turpis egestas pretium. Ac felis donec et odio pellentesque diam. Aliquam faucibus purus in massa tempor nec feugiat nisl. Dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu. Semper feugiat nibh sed pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliq.</p>
+    <p>Nicholas is a battle-hardened veteran of the OKC restaurant industry who is trying to reintegrate back into society by learning web development. In his personal time, he likes to worry about how little time he has. Sometimes he writes and hosts unconventional parties for his friends, like murder-mysteries, roasts, and "Attend Your Own Funeral" events. He is currently coping with <a href="https://i.redd.it/h7d4mzqxwga21.jpg">imposter syndrome</a> by pursuing his newfound passion for <a href="https://www.gnu.org/philosophy/floss-and-foss.en.html">FLOSS</a> (Free/Libre and Open-Source Software). He thinks he's cool because he installed Ubuntu on his PC.</p>
 </div>
 
 <div class="card2">
