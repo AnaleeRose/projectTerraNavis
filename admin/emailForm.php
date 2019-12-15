77da2dd19a652322ff7280a1e684745cd4bbaa33
Savannah S <analeeskinner@gmail.com>
2019-12-14 20:40:21 -0600

    misc changes

--- admin/emailForm.php
+++ admin/emailForm.php
@@ -51,132 +51,42 @@ if (!empty($_POST['email']) && !empty($_POST['msg'])) {
 
 function createEmail($subject, $msg) {
 $newMsg = '
-    <html>
-    <head>
-        <title>test</title>
-        <style type="text/css">
-            :root {
-                --color_1: grey;
-            }
 
-            html {
-                font-family: sans-serif;
-            }
-            body {
-                margin: 0;
-            }
-
-            .container {
-                padding: 1rem;
-            }
-
-
-            .header {
-                width: 85%;
-                margin: 0 auto 3rem;
-                max-width: 1300px;
-            }
-
-            .logo {
-                width: 6rem;
-            }
-        
-            .mainHeading {
-                font-size: 1.5rem;
-                margin-left: 1rem;
-            }
-
-            .headingSpace_large, .headingSpace_small {
-                opacity: 0;
-                font-size: .1px;
-                
-            }
-            
-                .headingSpace_large {width: 50%;}
-                .headingSpace_small {width: 5%;}
-
-            .unsubBtn {
-                border: 2px solid var(--color_1);
-                border-radius: 2rem;
-                padding: .8rem 1rem;
-                text-decoration: none;
-            }
-
-
-            .mainContent {
-                width: 80%;
-                margin: 0 auto;
-                max-width: 1200px;
-            }
-
-            .articleHeading {
-                font-size: 2rem;
-                font-weight: bold;
-                padding-bottom: 2rem;
-                text-align: center;
-            }
-
-            .picture {
-                margin: 0 auto;
-                width: 50%;
-            }
-
-            .content {
-                margin: 0 auto;
-                width: 40%;
-            }
-
-            .readLink {
-                text-align: right;
-            }
-
-            .margin {width: 5%;}
-
-            .team, .socialLinks, .contact {
-                width: 30vw;
-            }
-
-            .footer {
-                width: 100vw;
-                background: var(--color_1)
-            }
-
-        </style>
-    </head>
-    <body>
-        <table class="container">
-            <table class="header">
+<html>
+    <body style="font-family: sans-serif;">
+        <table class="container" style="padding: 1rem;">
+            <table class="header" style="width: 85%;margin: 0 auto 3rem;max-width: 1300px;">
                 <tr>
                     <th></th>
                     <th></th>
-                    <th class="headingSpace_large"></th>
+                    <th class="headingSpace_large" style="opacity: 0;font-size: .1px;width: 50%;"></th>
                     <th></th>
                 </tr>
                 <tr>
-                    <td><img class="logo" src="http://localhost/BPA/admin/assets/imgs/leaf_icon.jpg"></td>
-                    <td><h1 class="mainHeading">Terra Navis Living</td>
+                    <td><img class="logo" src="http://bpa-development.savannahskinner.com/admin/assets/imgs/leaf_icon.jpg" style="width: 6rem;"></td>
+                    <td><h1 class="mainHeading" style="font-size: 1.5rem;margin-left: 1rem;">Terra Navis Living</td>
                     <td></td>
-                    <td><a href="http://localhost/BPA/admin/" class="unsubBtn">Unsubscribe</a></td>
+                    <td><a href="http://bpa-development.savannahskinner.com/" class="unsubBtn" style="border: 2px solid grey;border-radius: 2rem;padding: .8rem 1rem;text-decoration: none;">Unsubscribe</a></td>
                 </tr>
             </table>
-            <table class="mainContent">
+            <table class="mainContent" style="width: 80%;margin: 0 auto;max-width: 1200px;">
                 <tr>
-                    <th class="margin"></th>
-                    <th class="picture"></th>
-                    <th class="content"></th>
-                    <th class="margin"></th>
+                    <th class="margin" style="width: 5%;"></th>
+                    <th class="picture" style="margin: 0 auto;width: 50%;"></th>
+                    <th class="content" style="margin: 0 auto;width: 40%;"></th>
+                    <th class="margin" style="width: 5%;"></th>
                 </tr>
 
                 <tr>
                     <td></td>
-                    <td class="articleHeading" colspan="2">' . $subject . '</td>
+                    <td class="articleHeading" colspan="2" style="font-size: 2rem;font-weight: bold;padding-bottom: 2rem;text-align: center;">' . $subject . '</td>
                     <td></td>
                     <td></td>
                 </tr>
 
                 <tr>
                     <td></td>
-                    <td><img class="articleImg" src="http://localhost/BPA/admin/assets/imgs/leaf_icon.jpg"></td>
+                    <td><img class="articleImg" src="http://bpa-development.savannahskinner.com/admin/assets/imgs/leaf_icon.jpg"></td>
                     <td><p class="articleDescription">' . $msg . '</p></td>
                     <td></td>
                 </tr>
@@ -184,42 +94,43 @@ $newMsg = '
                 <tr>
                     <td></td>
                     <td></td>
-                    <td><p class="readLink">Read More At Terra Navis >></p></td>
+                    <td><p class="readLink" style="text-align: right;">Read More At Terra Navis >></p></td>
                     <td></td>
                 </tr>
             </table>
         </table>
-        <table class="footer">
+        <table class="footer" style="width: 100vw;background: gray;">
             <tr>
-                <th class="team"></th>
-                <th class="socialLinks"></th>
-                <th class="contact"></th>
+                <th class="team" style="width: 30vw;"></th>
+                <th class="socialLinks" style="width: 30vw;"></th>
+                <th class="contact" style="width: 30vw;"></th>
             </tr>
             <tr>
                 <td class="teamContainer">
                     <h4 class="teamHeading">Meet Our Team</h4>
                     <p class="teamText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <a href="http://localhost/BPA/admin/" class="readLink">Read More At Terra Navis >></a>
+                    <a href="http://bpa-development.savannahskinner.com/" class="readLink">Read More At Terra Navis >></a>
                 </td>
                 <td>
                     <h3 class="footerArticleHeading">Read Our Latest Articles</h3>
-                    <a class="footerTNLink" href="http://localhost/BPA/admin/">Terra Navis Living</a>
+                    <a class="footerTNLink" href="http://bpa-development.savannahskinner.com/">Terra Navis Living</a>
                     <div class="socialLinkContainer">
                         <a href="facebok.com">fb</a>
                         <a href="twitter.com">twitter</a>
                         <a href="instagram.com">insta</a>
                     </div>
-                    
+
                 </td>
                 <td>
                     <p class="teamText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    
+
                 </td>
             </tr>
         </table>
 
     </body>
-    </html>';
+    </html>
+    ';
     return $newMsg;
 }
 
@@ -229,7 +140,7 @@ if ($sendflag) {
     $subject = "THIS IS THE SUBJECT";
     $msg = $_POST['msg'];
     $message = createEmail($subject, $msg);
-    $headers = "From: $from_email" . "\r\n" . "Reply-To: $email" . "\r\n" . $bcc_headers;
+    $headers = "From: $from_email" . "\r\n" . "Reply-To: $email" . "\r\n" . $bcc_headers .'Content-type: text/html; charset=iso-8859-1' . "\r\n";
     $a = mail( $mail_to_send_to, "Message from a contact form", $message, $headers );
     if ($a)
     {
--- admin/test.php
+++ admin/test.php
@@ -1,4 +1,83 @@
 <html>
+    <body style="font-family: sans-serif;">
+        <table class="container" style="padding: 1rem;">
+            <table class="header" style="width: 85%;margin: 0 auto 3rem;max-width: 1300px;">
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <th class="headingSpace_large" style="opacity: 0;font-size: .1px;width: 50%;"></th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <td><img class="logo" src="http://bpa-development.savannahskinner.com/admin/assets/imgs/leaf_icon.jpg" style="width: 6rem;"></td>
+                    <td><h1 class="mainHeading" style="font-size: 1.5rem;margin-left: 1rem;">Terra Navis Living</td>
+                    <td></td>
+                    <td><a href="http://bpa-development.savannahskinner.com/" class="unsubBtn" style="border: 2px solid grey;border-radius: 2rem;padding: .8rem 1rem;text-decoration: none;">Unsubscribe</a></td>
+                </tr>
+            </table>
+            <table class="mainContent" style="width: 80%;margin: 0 auto;max-width: 1200px;">
+                <tr>
+                    <th class="margin" style="width: 5%;"></th>
+                    <th class="picture" style="margin: 0 auto;width: 50%;"></th>
+                    <th class="content" style="margin: 0 auto;width: 40%;"></th>
+                    <th class="margin" style="width: 5%;"></th>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td class="articleHeading" colspan="2" style="font-size: 2rem;font-weight: bold;padding-bottom: 2rem;text-align: center;">' . $subject . '</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td><img class="articleImg" src="http://bpa-development.savannahskinner.com/admin/assets/imgs/leaf_icon.jpg"></td>
+                    <td><p class="articleDescription">' . $msg . '</p></td>
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td><p class="readLink" style="text-align: right;">Read More At Terra Navis >></p></td>
+                    <td></td>
+                </tr>
+            </table>
+        </table>
+        <table class="footer" style="width: 100vw;background: gray;">
+            <tr>
+                <th class="team" style="width: 30vw;"></th>
+                <th class="socialLinks" style="width: 30vw;"></th>
+                <th class="contact" style="width: 30vw;"></th>
+            </tr>
+            <tr>
+                <td class="teamContainer">
+                    <h4 class="teamHeading">Meet Our Team</h4>
+                    <p class="teamText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <a href="http://bpa-development.savannahskinner.com/" class="readLink">Read More At Terra Navis >></a>
+                </td>
+                <td>
+                    <h3 class="footerArticleHeading">Read Our Latest Articles</h3>
+                    <a class="footerTNLink" href="http://bpa-development.savannahskinner.com/">Terra Navis Living</a>
+                    <div class="socialLinkContainer">
+                        <a href="facebok.com">fb</a>
+                        <a href="twitter.com">twitter</a>
+                        <a href="instagram.com">insta</a>
+                    </div>
+
+                </td>
+                <td>
+                    <p class="teamText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                </td>
+            </tr>
+        </table>
+
+    </body>
+    </html>
+
+<!-- <html>
 <head>
 	<title>test</title>
 	<style type="text/css">
@@ -27,7 +106,7 @@
 		.logo {
 			width: 6rem;
 		}
-	
+
 		.mainHeading {
 			font-size: 1.5rem;
 			margin-left: 1rem;
@@ -36,9 +115,9 @@
 		.headingSpace_large, .headingSpace_small {
 			opacity: 0;
 			font-size: .1px;
-			
+
 		}
-		
+
 			.headingSpace_large {width: 50%;}
 			.headingSpace_small {width: 5%;}
 
@@ -156,14 +235,14 @@
 					<a href="twitter.com">twitter</a>
 					<a href="instagram.com">insta</a>
 				</div>
-				
+
 			</td>
 			<td>
 				<p class="teamText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				
+
 			</td>
 		</tr>
 	</table>
 
 </body>
-</html>
+</html> -->
