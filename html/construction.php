98246f541547c7a4c75e596de0341bc273a5fe27
Savannah S <analeeskinner@gmail.com>
2019-12-17 14:40:38 -0600

    css v1

--- html/construction.php
+++ html/construction.php
@@ -26,7 +26,7 @@ require './assets/includes/header.inc.php';
 
 	<section id="building">
 	    <h2 class="sec-title">Building Your Earthship</h2>
-	        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus cras adipiscing enim eu turpis egestas pretium. Ac felis donec et odio pellentesque diam. Aliquam faucibus purus in massa tempor nec feugiat nisl. Dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu. Semper feugiat nibh sed pulvinar. Sit amet consectetur adipiscing elit ut. Lacus sed turpis tincidunt id aliquet risus. Maecenas ultricies mi eget mauris pharetra et ultrices. Lorem ipsum dolor sit amet. Viverra justo nec ultrices dui sapien. Egestas maecenas pharetra convallis posuere. Mi sit amet mauris commodo quis imperdiet massa tincidunt.</p>
+	      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus cras adipiscing enim eu turpis egestas pretium. Ac felis donec et odio pellentesque diam. Aliquam faucibus purus in massa tempor nec feugiat nisl. Dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu. Semper feugiat nibh sed pulvinar. Sit amet consectetur adipiscing elit ut. Lacus sed turpis tincidunt id aliquet risus. Maecenas ultricies mi eget mauris pharetra et ultrices. Lorem ipsum dolor sit amet. Viverra justo nec ultrices dui sapien. Egestas maecenas pharetra convallis posuere. Mi sit amet mauris commodo quis imperdiet massa tincidunt.</p>
 	</section>
 
 	<section id="building-code">
