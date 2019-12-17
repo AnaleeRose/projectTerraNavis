f6339a09f2e7256c067c9402666e01e075425040
Savannah S <analeeskinner@gmail.com>
2019-12-17 16:49:00 -0600

    footer (85%)

--- html/assets/includes/footer.inc.php
+++ html/assets/includes/footer.inc.php
@@ -2,34 +2,60 @@
     <div id="f-container">
         <div id="team-blurb" class="box-lg box-1">
             <h2>Meet Our Team</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna alqua. Ut enim ad minim veniam, quis nostrud magna aliqua.</p>
-            <a href="team.html" class="blurb-link">Read More >></a>
+            <p>Jerad, Nick, Noshin, and Savannah. Four students from Francis Tuttle Technology Center who banded to gether to build a site. We created Terra Navis in hopes of offering a resource to the community while improving their skills. Want to learn more about us and our individual stories?</p>
+            <a href="team.html" class="blurb-link">Meet Our Team >></a>
         </div>
         <div id=join class="box-lg box-2">
             <form method="post">
                 <label class="h2" for="email">Join Our Newsletter!</label>
                 <div class="emailContainer">
                     <input id="email" name="email" type="email" placeholder="email@example.com">
-                    <button type="submit" class="newsButton">&#10003;</button>
+                    <button type="submit" class="newsButton">Sign Up For Newsletter</button>
                 </div>
             </form>
-            <div class="icons">
-                <a href="<?= FACEBOOK_LINK; ?>"><img id="fb-icon" src="images/facebook-square.png" alt="Facebook Icon"></a>
-                <a href="<?= TWITTER_LINK; ?>"><img id="twitter-icon" src="images/twitter-square.png" alt="Twitter Icon"></a>
-                <a href="<?= PININTEREST_LINK; ?>"><img id="pin-icon" src="images/instagram.png" alt="Pinterest Icon"></a>
+            <div class="socialLinkIconContainer" alt="Francis Tuttle Facebook Account" title="Facebook">
+                <a href="<?= FACEBOOK_LINK; ?>">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="65" height="65" viewBox="0 0 65 65" class="fb_icon">
+                      <path id="facebook-square" d="M58.036,32H6.964A6.964,6.964,0,0,0,0,38.964V90.036A6.964,6.964,0,0,0,6.964,97H26.878V74.9H17.737V64.5h9.141V56.572c0-9.017,5.368-14,13.591-14a55.376,55.376,0,0,1,8.055.7v8.85H43.987c-4.47,0-5.865,2.774-5.865,5.619V64.5H48.1l-1.6,10.4H38.122V97H58.036A6.964,6.964,0,0,0,65,90.036V38.964A6.964,6.964,0,0,0,58.036,32Z" transform="translate(0 -32)"/>
+                    </svg>
+                </a>
+                <a href="<?= TWITTER_LINK; ?>" alt="Francis Tuttle Twitter Account" title="Twitter">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="65" height="65" viewBox="0 0 65 65" class="twitter_icon">
+                      <path id="twitter-square" d="M58.036,32H6.964A6.966,6.966,0,0,0,0,38.964V90.036A6.966,6.966,0,0,0,6.964,97H58.036A6.966,6.966,0,0,0,65,90.036V38.964A6.966,6.966,0,0,0,58.036,32ZM50.941,55.04c.029.406.029.827.029,1.233,0,12.579-9.576,27.074-27.074,27.074a26.949,26.949,0,0,1-14.61-4.266,20.047,20.047,0,0,0,2.292.116,19.078,19.078,0,0,0,11.81-4.062,9.528,9.528,0,0,1-8.894-6.6,10.256,10.256,0,0,0,4.295-.174,9.516,9.516,0,0,1-7.617-9.344V58.9a9.509,9.509,0,0,0,4.295,1.2,9.5,9.5,0,0,1-4.237-7.922,9.4,9.4,0,0,1,1.291-4.8,27.019,27.019,0,0,0,19.616,9.953,9.536,9.536,0,0,1,16.235-8.691,18.647,18.647,0,0,0,6.036-2.292,9.491,9.491,0,0,1-4.179,5.238,18.936,18.936,0,0,0,5.484-1.48A20.029,20.029,0,0,1,50.941,55.04Z" transform="translate(0 -32)"/>
+                    </svg>
+                </a>
+                <a href="<?= PININTEREST_LINK; ?>" alt="Pininterest Link" title="Pininterest">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="65" height="64.985" viewBox="0 0 65 64.985" class="pin_icon">
+                      <path id="instagram" d="M32.432,47.656A16.661,16.661,0,1,0,49.094,64.318,16.635,16.635,0,0,0,32.432,47.656Zm0,27.494A10.832,10.832,0,1,1,43.264,64.318,10.852,10.852,0,0,1,32.432,75.15ZM53.661,46.975a3.886,3.886,0,1,1-3.886-3.886A3.877,3.877,0,0,1,53.661,46.975ZM64.7,50.919C64.45,45.713,63.261,41.1,59.447,37.3s-8.41-4.988-13.616-5.249c-5.365-.3-21.447-.3-26.812,0-5.191.247-9.8,1.436-13.616,5.235S.414,45.7.153,50.9c-.3,5.365-.3,21.447,0,26.812C.4,82.922,1.589,87.534,5.4,91.333s8.41,4.988,13.616,5.249c5.365.3,21.447.3,26.812,0,5.206-.247,9.817-1.436,13.616-5.249s4.988-8.41,5.249-13.616c.3-5.365.3-21.432,0-26.8ZM57.765,83.473a10.967,10.967,0,0,1-6.177,6.177c-4.278,1.7-14.428,1.305-19.156,1.305s-14.892.377-19.156-1.305A10.967,10.967,0,0,1,7.1,83.473C5.4,79.2,5.794,69.045,5.794,64.318S5.417,49.425,7.1,45.162a10.967,10.967,0,0,1,6.177-6.177c4.278-1.7,14.428-1.305,19.156-1.305s14.892-.377,19.156,1.305a10.967,10.967,0,0,1,6.177,6.177c1.7,4.278,1.305,14.428,1.305,19.156S59.462,79.21,57.765,83.473Z" transform="translate(0.075 -31.825)"/>
+                    </svg>
+                </a>
             </div>
         </div>
         <div id="contact-info" class="box-lg box-3">
-            <div class="address box-sm">
-                <img id="home-icon" src="images/" alt="Home Icon">
-                <p>12777 N Rockwell Ave.<br>Oklahoma City, OK 73142</p>
+            <div class="address_box">
+                <svg id="Group_2" data-name="Group 2" xmlns="http://www.w3.org/2000/svg" width="47.689" height="37.952" viewBox="0 0 47.689 37.952" class="houseIcon">
+                  <path id="Path_3" data-name="Path 3" d="M82.736,120.939,65.728,134.96a.4.4,0,0,1-.015.089.4.4,0,0,0-.015.088v14.2a1.921,1.921,0,0,0,1.893,1.894H78.949V139.87h7.573v11.359H97.881a1.923,1.923,0,0,0,1.894-1.894v-14.2a.416.416,0,0,0-.03-.177Z" transform="translate(-58.891 -113.278)" />
+                  <path id="Path_4" data-name="Path 4" d="M47.36,65.482,40.882,60.1V48.03a.912.912,0,0,0-.947-.946H34.256a.911.911,0,0,0-.947.946V53.8l-7.217-6.034a3.669,3.669,0,0,0-4.5,0L.328,65.482A.859.859,0,0,0,0,66.117a.958.958,0,0,0,.207.7L2.044,69a1.009,1.009,0,0,0,.621.326,1.1,1.1,0,0,0,.71-.207L23.844,52.052,44.313,69.12a.912.912,0,0,0,.621.206h.089A1.01,1.01,0,0,0,45.645,69l1.834-2.189a.957.957,0,0,0,.206-.7A.862.862,0,0,0,47.36,65.482Z" transform="translate(0.001 -46.994)" />
+                </svg>
+                <p>12777 N Rockwell Ave,<br> Oklahoma City, OK 73142</p>
             </div>
-            <div class="phone box-sm">
-                <img id="mobile-icon" src="images/" alt="Mobile Icon">
+
+            <div class="phone_box">
+                <svg xmlns="http://www.w3.org/2000/svg" width="29.422" height="50" viewBox="0 0 29.422 50" class="phoneIcon">
+                  <g id="smartphone-call" transform="translate(-7.334)">
+                    <path id="Path_1" data-name="Path 1" d="M33.335,0H10.755A3.409,3.409,0,0,0,7.334,3.377V46.621A3.41,3.41,0,0,0,10.755,50h22.58a3.408,3.408,0,0,0,3.421-3.377V3.377A3.409,3.409,0,0,0,33.335,0Zm-14.9,2.434h7.224a.409.409,0,1,1,0,.819H18.433a.409.409,0,1,1,0-.819Zm3.612,45.877a1.689,1.689,0,1,1,1.71-1.69A1.7,1.7,0,0,1,22.045,48.311ZM34.376,43.75H9.714V5.356H34.376Z"/>
+                  </g>
+                </svg>
+
                 <p>1-888-867-5309</p>
             </div>
-            <div class="email box-sm">
-                <img id="email-icon" src="images/" alt="Email Icon">
+            <div class="email_box">
+                <svg xmlns="http://www.w3.org/2000/svg" width="45.919" height="31.943" viewBox="0 0 45.919 31.943" class="emailIcon">
+                  <g id="email-envelope-outline" transform="translate(188 179.889)">
+                    <path id="Path_2" data-name="Path 2" d="M45.42,59.111H.5a.5.5,0,0,0-.5.5V90.556a.5.5,0,0,0,.5.5H45.42a.5.5,0,0,0,.5-.5V59.61A.5.5,0,0,0,45.42,59.111Zm-7.6,3.993L22.96,74,8.1,63.1Zm4.1,23.957H3.993V65.048L22.664,78.74a.5.5,0,0,0,.592,0L41.927,65.048V87.061Z" transform="translate(-188 -239)"/>
+                  </g>
+                </svg>
+
                 <p><?= EMAIL_LINK ?></p>
             </div>
         </div>
