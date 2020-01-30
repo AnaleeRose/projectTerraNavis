addb26c01fd90942ef751c14ff68e15949de2290
Nick Ferrari <njferrari14@gmail.com>
2020-01-30 08:57:07 -0600

    removed jerad's commented out bio, fixed nick's portfolio link

--- html/team.php
+++ html/team.php
@@ -35,21 +35,9 @@ require './assets/includes/head.php';
                 <h3 class="mainSection-heading subheading">Nicholas Ferrari</h3>
                 <h4 class="role">Project Manager & SEO</h4>
                 <p class="description">Nicholas is a battle-hardened veteran of the OKC restaurant industry who is trying to reintegrate back into society by learning web development. In his personal time, he likes to worry about how little time he has. Sometimes he writes and hosts unconventional parties for his friends, like murder-mysteries, roasts, and "Attend Your Own Funeral" events. He is currently coping with <a href="https://i.redd.it/h7d4mzqxwga21.jpg">imposter syndrome</a> by pursuing his newfound passion for <a href="https://www.gnu.org/philosophy/floss-and-foss.en.html">FLOSS</a> (Free/Libre and Open-Source Software). He thinks he's cool because he installed Ubuntu on his PC.</p>
-                <a class="portfolioLink" href="yourportfolio.com">His Portfolio >></a>
+                <a class="portfolioLink" href="http://site12.wdd.francistuttle.edu/index.html">His Portfolio >></a>
             </div>
         </section>
-
-<!--         <section id="mainSection-two" class="mainSection-container">
-            <div class="portrait-container">
-                <img class="portrait" src="assets/images/temp.png" alt="Portrait">
-            </div>
-            <div class="mainSection-content">
-                <h3 class="mainSection-heading subheading">Jerad Bowles</h3>
-                <h4 class="role">Front-end Developer & Content Specialist</h4>
-                <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus cras adipiscing enim eu turpis egestas pretium. Ac felis donec et odio pellentesque diam. Aliquam faucibus purus in massa tempor nec feugiat nisl. Dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu. Semper feugiat nibh sed pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliq.</p>
-                <a class="portfolioLink" href="yourportfolio.com">His Portfolio >></a>
-            </div>
-        </section> -->
 
         <section id="mainSection-two" class="mainSection-container">
             <div class="portrait-container">
